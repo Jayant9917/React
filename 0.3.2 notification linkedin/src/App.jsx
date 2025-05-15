a655099b9f3055a5e192f3759a7519f44661def5
Jayant9917 <ranajayant527@gmail.com>
2025-05-15 15:24:53 +0530

    useEffect

--- 0.3.2 notification linkedin/src/App.jsx
+++ 0.3.2 notification linkedin/src/App.jsx
@@ -4,31 +4,20 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(1);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  function increaseCount(){
+    setCount(count + 1);
+  }
+  return ( <div>
+    <div style={{display: "flex" }}>
+      <div style={{background: "red", boarderRadius: 0, width:20
+        , height: 25, paddingLeft: 10, paddingTop: 5}}>
+          {count}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+    <img style={{cursor: "pointer"}} src={reactLogo} alt="react" onClick={increaseCount}/>
+    </div>
   )
 }
 
